446d8aef545ec70faa7b31a2aec5e248eeda798e
chetandivekar <chetandivekar6@gmail.com>
2023-07-12 18:02:27 +0530

    hero section added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "./App.css";
-import SelectCategory from "./components/Category/SelectCategory";
 import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      <div>
-        <Hero />
-      </div>
-      <SelectCategory/>
+      <Hero />
     </div>
   );
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,31 @@
 import React from "react";
+import "./Hero.css";
+import LeftLogo from "./HeroImages/left.png";
+import RightLogo from "./HeroImages/right.png";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="hero-main">
+      <div className="hero">
+        <div className="hero__left">
+          <img src={LeftLogo} alt="img" />
+        </div>
+        <div className="hero__middle">
+          <div className="heading">
+            Find & Search <br /> You Dream School
+          </div>
+          <div className="sub-heading">
+            "Dream big, excel beyond limits - Welcome to Your Dream School!"
+          </div>
+          <div className="button">
+            <input type="tel" id="phone" name="phone" placeholder="Search..." />
+            <button>Search</button>
+          </div>
+        </div>
+        <div className="hero__right">
+          <img src={RightLogo} alt="img" />
+        </div>
+      </div>
+    </div>
+  );
 }
