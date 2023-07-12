5d42ded6f2548a4d0a3441c2349d933af87830f8
itsKapri <nk92042@gmail.com>
2023-07-12 13:05:06 +0530

    name added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello this is Campus Finder</h1>
-      <h2>hii from pump</h2>
+      <h2>hii from Nilesh</h2>
       <h3>Hey Beta</h3>
     </div>
   );
