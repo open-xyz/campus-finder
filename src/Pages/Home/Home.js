09b7c0f20b58b9508cce0ede07de0fff4931c8ef
chetandivekar <chetandivekar6@gmail.com>
2023-07-13 21:24:49 +0530

    ranking added, UI improved

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./Home.css";
 import Hero from "./components/Hero/Hero";
+import Ranking from "./Topranking/Topranking";
 import SelectCategory from "./components/Category/SelectCategory";
 
 export default function Home() {
   return (
-    <div>
+    <div className="app">
       <Hero />
       <SelectCategory />
+      <Ranking />
     </div>
   );
 }
---
+++ src/Pages/Home/Topranking/Topranking.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Topranking.css";
+
+export default function Topranking() {
+  return (
+    <div className="top-section">
+      <div className="school-ranking">
+        <h1>Top School</h1>
+        <p>in Mumbai</p>
+      </div>
+
+      <div className="college-ranking">
+        <h1>Top College</h1>
+        <p>in Mumbai</p>
+      </div>
+    </div>
+  );
+}
