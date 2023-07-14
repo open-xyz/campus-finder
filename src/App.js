b50f377df7e53e1852f2c79a8227ae25b3e6c328
IAMBHUSHANCHAVAN <bhushanchavan976@gmail.com>
2023-07-14 13:36:41 +0530

    search bar done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Pages/Home/Home";
 import College from "./Pages/College/College";
 import Navbar from "./Pages/Navbar/Navbar/Navbar";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import School from "./Pages/School/School";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
   {
     path: "/college",
     element: <College />,
+  },
+  {
+    path: "/school",
+    element: <School/>,
   },
 ]);
 
---
+++ src/Pages/School/School.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "../School/School.css"
+import "../Home/components/Hero/Hero.css"
+// import { TypeAnimation } from "react-type-animation";
+import Arrow from "../Home/components/Hero/HeroImages/arrow.svg"
+import Search from "../Home/components/Hero/HeroImages/search.svg"
+const School = () => {
+  return (
+    <div>
+      <div className="school-section">
+      <div className="school-filter">
+      </div>
+      <div className="school-main">
+      <div className="school-search">
+      <h1>Search School</h1>
+      <div className="hero__middle">
+          <div className="search-bar-school">
+            <div className="custom-select">
+              <select>
+                <option value="">All</option>
+                <option value="">Schools</option>
+                <option value="">Colleges</option>
+              </select>
+              <span className="custom-background"></span>
+              <span className="custom-arrow">
+                <img src={Arrow} alt="arrow" />
+              </span>
+            </div>
+            <div className="line"></div>
+            <img style={{ marginRight: "0.8rem" }} src={Search} alt="" />
+            <input type="tel" id="phone" name="phone" placeholder="Search..." />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+      <div className="school-result">
+      search result
+      </div>
+      </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default School
