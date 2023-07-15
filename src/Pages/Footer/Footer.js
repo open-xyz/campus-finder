e228dd17fe88d17cc8ccf246c4dbae09ecd5229a
chetandivekar <chetandivekar6@gmail.com>
2023-07-15 23:57:40 +0530

    footer  adjusted

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer className=" rounded-lg shadow dark:bg-gray-900 m-4">
+      <footer
+        style={{ marginTop: "3rem" }}
+        className=" rounded-lg shadow dark:bg-gray-900 m-4"
+      >
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a
